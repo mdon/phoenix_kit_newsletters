1cd3024b5129b3108917ad6087879bead2a4d96a
Dmitri Don <fotkin@gmail.com>
2026-05-25 21:45:04 +0000

    libs upgraded

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule PhoenixKitNewsletters.MixProject do
     [
       quality: ["format", "credo --strict", "dialyzer"],
       "quality.ci": ["format --check-formatted", "credo --strict", "dialyzer"],
-      precommit: ["compile", "quality"]
+      precommit: ["compile --force --warnings-as-errors", "deps.unlock --check-unused", "quality.ci"]
     ]
   end
 
